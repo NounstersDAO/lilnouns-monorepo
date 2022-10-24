68be2b82948cd8f8e7965ef04ee053770264b858
NounstersDAO <113967350+NounstersDAO@users.noreply.github.com>
2022-10-24 12:26:59 -0700

    Delete packages/nouns-webapp directory

--- packages/nouns-webapp/src/components/AuctionActivityWrapper/index.tsx
+++
@@ -1,7 +0,0 @@
-import classes from './AuctionActivityWrapper.module.css';
-
-// eslint-disable-next-line @typescript-eslint/ban-types
-const AuctionActivityWrapper: React.FC<{}> = props => {
-  return <div className={classes.wrapper}>{props.children}</div>;
-};
-export default AuctionActivityWrapper;
